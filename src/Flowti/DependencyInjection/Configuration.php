56895a498a9d6e57fb372f9082802e38c3584d42
Thomas Sens <thomas.sens@flowti.com.br>
2022-01-04 18:45:38 -0300

    teste

--- src/Flowti/DependencyInjection/Configuration.php
+++ src/Flowti/DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('zabbix');
+        $treeBuilder = new TreeBuilder('flowti_zabbix');
 
         $treeBuilder->getRootNode()
         ->children()
