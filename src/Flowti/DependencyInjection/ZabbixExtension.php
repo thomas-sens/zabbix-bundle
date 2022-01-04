b9e0d3020a0bd78a1f177a6b01d62d0092c51544
Thomas Sens <thomas.sens@flowti.com.br>
2022-01-04 16:51:09 -0300

    novo

--- src/Flowti/DependencyInjection/ZabbixExtension.php
+++ src/Flowti/DependencyInjection/ZabbixExtension.php
@@ -15,10 +15,38 @@ class ZabbixExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
+    
+        $this->recursiveSettingContainerParameters($container, ['flowti_zabbix'], $configs);
+        /*
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         foreach ($config as $key => $value) {
             $container->setParameter('flowti_zabbix.' . $key, $value);
+        }
+        */
+    }
+
+    /**
+     * This function is providing the configuration parameters
+     * to the container for retrieving the variables by calling $container->getParamter('')
+     *
+     * @param       $container
+     * @param array $pathArray
+     * @param array $array
+     */
+    protected function recursiveSettingContainerParameters(&$container, array $pathArray, array $array)
+    {
+        foreach ($array AS $key => $value) {
+            /**
+             * this step could cause problems when we have a configuration schema which
+             * requires to set an array as values
+             */
+            if (is_array($value)) {
+                $pathArray[] = $key;
+                $this->recursiveSettingContainerParameters($container, $pathArray, $value);
+            } else {
+                $container->setParameter(implode('.', $pathArray) . '.' . $key, $value);
+            }
         }
     }
 }
---
+++ src/Flowti/Service/FlowtiZabbixClient.php
@@ -0,0 +1,74 @@
+<?php
+
+    namespace Flowti\ZabbixBundle\Service;
+
+    use Symfony\Component\DependencyInjection\ContainerInterface;
+    use Symfony\Component\HttpClient\HttpClient;
+
+    class AmacadoZabbixClient
+    {
+        protected $httpClient;
+        protected $zabbixCredentialsHost;
+        protected $zabbixCredentialsUsername;
+        protected $zabbixCredentialsPassword;
+
+        public function __construct(ContainerInterface $container)
+        {
+            $this->container  = $container;
+            $this->httpClient = HttpClient::create([
+                'headers' => [
+                    'Content-Type' => 'application/json-rpc',
+                ]
+            ]);
+
+            $this->zabbixCredentialsHost     = $this->container->getParameter('flowti_zabbix.client.host');
+            $this->zabbixCredentialsUsername = $this->container->getParameter('flowti_zabbix.client.username');
+            $this->zabbixCredentialsPassword = $this->container->getParameter('flowti_zabbix.client.password');
+        }
+
+        public function login()
+        {
+            $body          = new \stdClass();
+            $body->jsonrpc = '2.0';
+            $body->method  = 'user.login';
+
+            $params           = new \stdClass();
+            $params->user     = $this->zabbixCredentialsUsername;
+            $params->password = $this->zabbixCredentialsPassword;
+            $body->params     = $params;
+
+            $body->id   = 1;
+            $body->auth = null;
+
+            $jsonEncodedBody = json_encode($body);
+
+            return $this->httpClient->request('POST', $this->zabbixCredentialsHost, ['body' => $jsonEncodedBody]);
+        }
+
+        public function getHappyMessage()
+        {
+
+
+            $response = $this->login();
+            var_dump($response->getContent());
+            exit();
+            var_dump($this->container->getParameter('flowti_zabbix.client.host'));
+            echo '<br />';
+            var_dump($this->container->getParameter('flowti_zabbix.client.username'));
+            echo '<br />';
+            var_dump($this->container->getParameter('flowti_zabbix.client.password'));
+            echo '<br />';
+
+            exit();
+
+            $messages = [
+                'You did it! You updated the system! Amazing!',
+                'That was one of the coolest updates I\'ve seen all day!',
+                'Great work! Keep going!',
+            ];
+
+            $index = array_rand($messages);
+
+            return $messages[$index];
+        }
+    }
