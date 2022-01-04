283aae6ebbefb57ce26f1c9a8e4491e332fc2d7c
Thomas Sens <thomas.sens@flowti.com.br>
2022-01-04 18:16:56 -0300

    teste

--- src/Flowti/DependencyInjection/ZabbixExtension.php
+++ src/Flowti/DependencyInjection/ZabbixExtension.php
@@ -3,20 +3,20 @@ namespace Flowti\ZabbixBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 /**
  * ZabbixExtension
  */
-class ZabbixExtension extends Extension
+class ZabbixExtension extends ConfigurableExtension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
     
-        $this->recursiveSettingContainerParameters($container, ['flowti_zabbix'], $configs);
+        $this->recursiveSettingContainerParameters($container, ['flowti_zabbix'], $mergedConfig);
         /*
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
